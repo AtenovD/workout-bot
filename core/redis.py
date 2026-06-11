e7c32429536a90069c6375857c2f1f618733cdb4
Atenov_D <krysanov2507@gmail.com>
2026-06-11 15:31:48 +0300

    feat: add core/redis.py

---
+++ core/redis.py
@@ -0,0 +1,7 @@
+import redis.asyncio as aioredis
+from core.config import settings
+
+redis_client: aioredis.Redis = aioredis.from_url(settings.redis_url, encoding="utf-8", decode_responses=True)
+
+async def get_redis() -> aioredis.Redis:
+    return redis_client
