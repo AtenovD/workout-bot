9c77975289f63246f1f653b43f3f8b214250f3c0
Atenov_D <krysanov2507@gmail.com>
2026-06-11 15:31:47 +0300

    feat: add core/db.py

---
+++ core/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from core.config import settings
+
+engine = create_async_engine(settings.database_url, pool_size=10, max_overflow=20)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
