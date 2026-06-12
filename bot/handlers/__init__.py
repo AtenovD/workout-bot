5ea52c191aa8ea0d7d50b236ad1681662f24f0d9
Atenov_D <krysanov2507@gmail.com>
2026-06-13 01:34:10 +0300

    fix: remove start_router import (start.py does not exist)

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,4 +1,6 @@
-from .start import router as start_router
+# Bot handlers package
+# Note: onboarding.py handles /start and initial calibration flow
+
 from .workout import router as workout_router
 from .equipment import router as equipment_router
 from .schedule import router as schedule_router
@@ -13,8 +15,8 @@ from .help import router as help_router
 from .admin import router as admin_router
 
 __all__ = [
-    "start_router", "workout_router", "equipment_router",
-    "schedule_router", "profile_router", "calibration_router",
-    "gamification_router", "reminder_router", "measurements_router",
-    "referral_router", "settings_router", "help_router", "admin_router",
+    "workout_router", "equipment_router", "schedule_router",
+    "profile_router", "calibration_router", "gamification_router",
+    "reminder_router", "measurements_router", "referral_router",
+    "settings_router", "help_router", "admin_router",
 ]
